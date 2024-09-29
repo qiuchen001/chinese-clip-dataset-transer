1b853e19ec8f6806911ee9312f0c6b5f27a2a926
zhangchenchen <zhangchenchen@51aes.com>
2024-09-29 17:41:41 +0800

    画出目标数据

--- bdd100k/draw_image.py
+++ bdd100k/draw_image.py
@@ -1,7 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
+from PIL import Image
 
 def draw_image(image_labels_file_path, image_file_path, draw_image_path):
     # 示例JSON数据
@@ -37,6 +37,9 @@ def draw_image(image_labels_file_path, image_file_path, draw_image_path):
                 # 添加类别标签
                 ax.text(x1, y1, obj['category'], color='r', fontsize=8)
 
+                # 切出目标框并保存为新的图片
+                crop_image(image_file_path, x1, y1, x2, y2, obj['category'])
+
     # 设置图形属性
     ax.set_aspect('equal')
 
@@ -46,7 +49,19 @@ def draw_image(image_labels_file_path, image_file_path, draw_image_path):
     # 显示图形
     plt.show()
 
+def crop_image(image_file_path, x1, y1, x2, y2, category):
+    # 加载图片
+    image = Image.open(image_file_path)
 
+    # 切出目标框
+    cropped_image = image.crop((x1, y1, x2, y2))
+
+    # 生成新的图片名称，包含类别名称
+    cropped_image_path = f"{category}_{x1}_{y1}_{x2}_{y2}.jpg"
+
+    # 保存切出的图片
+    cropped_image.save(cropped_image_path)
+    print(f"Saved cropped image: {cropped_image_path}")
 
 if __name__ == "__main__":
     image_labels_file_path = r"E:\playground\ai\datasets\0000f77c-6257be58.json"
